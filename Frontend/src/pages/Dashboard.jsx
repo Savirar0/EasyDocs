42446f8417a313160f2fcee47964e67da4804fab
Navadeep <navdeep2666@gmail.com>
2026-09-16 00:52:43 +0530

    feat: Added donut chart in dashboard.

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,66 @@
 import 'react'
-
-
+import { useState } from 'react'
+import { useAuth } from '@/context/auth'
+import { DonutChart } from "@/components/DonutChart"
 
 function Dashboard() {
     
+    const {role, companyId, companyName, token} = useAuth();
+
+    const[totalEmp, setTotalEmp] = useState(null);
+    const[managementCount, setManagementCount] = useState(null);
+    const[employeeCount, setEmployeeCount] = useState(null);
+
+    const getCount = async ()=>{
+        if (role!="MANAGEMENT"){
+            return{
+                success: false,
+                error: "You do not have access to this data."
+            }
+        }else{
+            try {
+                const response = await fetch('/api/company-employee-count/', {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+                });
+                if (!response.ok) {
+                throw new Error('Failed to fetch data');
+                }
+                const data = await response.json();
+                setTotalEmp(data?.Total_Company_Employees)
+                setManagementCount(data?.Management)
+                setEmployeeCount(data?.Employees)
+            } catch (error) {
+                console.error('Dashboard Fetch Error:', error);
+            }
+        }
+    }
+
+    const chartData = [
+        {
+            name: "Total Employees",
+            amount: totalEmp
+        },
+        {
+            name: "Management",
+            amount: managementCount
+        },
+        {
+            name: "Employees",
+            amount: employeeCount
+        },
+    ]
+
     return (
         <>
-
+            <DonutChart
+                className="mx-auto"
+                data={chartData}
+                category="name"
+                value="amount"
+                showLabel={true}
+            />
         </>
     )
 }
